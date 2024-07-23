984fed5222a03e64563386943aa1703828e7c889
Devesh Bhardwaj <b23cs1014@iitj.ac.in>
2024-07-23 13:12:10 +0530

    "Added the Quiz Section"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import CompareSlider from './components/CompareSlider';
+import DesignQuiz from './components/DesignQuiz';
 
 function App() {
   return (
     <div className="App">
       <CompareSlider/>
+      <DesignQuiz/>
     </div>
   );
 }
