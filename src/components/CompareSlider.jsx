852a4b8459a112f0ff0225f264523b7d493a7373
Devesh Bhardwaj <b23cs1014@iitj.ac.in>
2024-07-24 17:29:04 +0530

    MInor changes and animations added

--- src/components/CompareSlider.jsx
+++ src/components/CompareSlider.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './CompareSlider.css'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { ImgComparisonSlider } from '@img-comparison-slider/react';
+import { motion,useInView, useAnimationControls} from 'framer-motion'
 import { Link } from 'react-router-dom';
 function CompareSlider() {
+  const ref = useRef();
+  const ref1 = useRef();
+  const controls = useAnimationControls();
+  const controls1 = useAnimationControls();
+  const isInview = useInView(ref);
+  const isInview1 = useInView(ref1);
+  useEffect(()=>{
+    if(isInview){
+        controls.start({scaleX:1,scaleY:1,y:0,opacity:1,transition:{duration:0.7, type:'spring'}})
+    }
+  },[isInview,controls])
+  useEffect(()=>{
+    if(isInview1){
+        controls1.start({scaleX:1,scaleY:1,y:0,opacity:1,transition:{duration:0.4, type:'keyframes'}})
+    }
+  },[isInview1,controls1])
   const [project_number, setProjectNumber] = useState(1);
   // Image reesource can be changed after creating the asset folder
   const left_image_resource = ["https://images.pexels.com/photos/2883049/pexels-photo-2883049.jpeg?auto=compress&cs=tinysrgb&w=600", "https://images.pexels.com/photos/2251247/pexels-photo-2251247.jpeg?auto=compress&cs=tinysrgb&w=600", "https://images.pexels.com/photos/5644353/pexels-photo-5644353.jpeg?auto=compress&cs=tinysrgb&w=600", "https://images.pexels.com/photos/23916863/pexels-photo-23916863/free-photo-of-luxury-interior-design.png?auto=compress&cs=tinysrgb&w=600", "https://images.pexels.com/photos/23916869/pexels-photo-23916869/free-photo-of-interior-design-of-luxury-living-room.png?auto=compress&cs=tinysrgb&w=600", "https://images.pexels.com/photos/23916837/pexels-photo-23916837/free-photo-of-pool-in-house.png?auto=compress&cs=tinysrgb&w=600"]
@@ -23,25 +40,25 @@ function CompareSlider() {
       </div>
 
       {/* Project title bar */}
-      <div className='SliderProjectTitleBar'>
+      <motion.div initial={{scaleX:0,y:20,scaleY:0,opacity:1}} animate={controls1} ref={ref1} className='SliderProjectTitleBar'>
         {ProjectTitles.map((title, index) => (
           <div className="ProjectTItles" key={index + 1} onClick={() => { setProjectNumber(index + 1) }}>{title}</div>
         ))}
 
         {/* TODO: Insert the Link Here for navigating the user to the gallery */}
-        <div style={{marginLeft:'10px', marginRight:'10px'}}>Explore More+</div>
-      </div>
+        <div style={{ marginLeft: '10px', marginRight: '10px' }}>Explore More+</div>
+      </motion.div>
 
       {/* Comparison Slider */}
-      <div className='ComparisonSliderCont'>
+      <motion.div initial={{scaleX:0,scaleY:0,opacity:0}} animate={{scaleX:1,scaleY:1,opacity:1,transition:{duration:0.7,type:'spring'}}} className='ComparisonSliderCont'>
         <ImgComparisonSlider className='ComparisonSlider'>
-          <img slot="first" src={left_image_resource[project_number-1]} className='compImages'/>
-          <img slot="second" src={right_image_resource[project_number-1]} className='compImages' />
+          <img slot="first" src={left_image_resource[project_number - 1]} className='compImages' alt='image1'/>
+          <img slot="second" src={right_image_resource[project_number - 1]} className='compImages' alt='image1'/>
         </ImgComparisonSlider>
-      </div>
+      </motion.div>
 
       {/* Start Transform Button */}
-      <Link to={"design_quiz"} className='SliderBottomButton'>START MY TRANSFORMATION</Link>
+      <motion.div initial={{scaleX:0,scaleY:0,y:80,opacity:0}} animate={controls} ref={ref} className='SliderBottomButton'><Link to={"design_quiz"} style={{textDecoration:'none',color:"white"}} >START MY TRANSFORMATION</Link></motion.div>
     </div>
   )
 }
