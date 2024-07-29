8763b7eedd6ec79beec1de433a0c0aaaae570a89
Mayuri R. Pujari <141871018+Mayu-infinite@users.noreply.github.com>
2024-07-29 10:16:52 +0530

    Update App.js
    Updated this section by adding function calling OurServices, WhyUs, FiveSteps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import './App.css';
+import OurServices from './components/OurServices';
+import WhyUs from './components/WhyUs';
+import FiveSteps from './components/FiveSteps';
 import CompareSlider from './components/CompareSlider';
 import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <OurServices />
+      <WhyUs />
+      <FiveSteps />
+    </div>
+  );
+  
 function App() {
   return (
     <div className="App">
