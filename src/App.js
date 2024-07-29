d1186fefe069ebe7687321293b911650025bfc27
deek-shan-t <singhdekson@gmail.com>
2024-07-29 18:36:37 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
 import './App.css';
-<<<<<<< HEAD
-import AboutUs from './components/about_us';
-import CompareSlider from './components/CompareSlider';
-import Footer from './components/Footer';
-import QuoteForm from './components/form';
-import Header from './components/nav_bar';
-import Navbar from './components/nav_bar';
-function App() {
-  return (
-    <div className="App">
-      
-=======
 import OurServices from './components/OurServices';
 import WhyUs from './components/WhyUs';
 import FiveSteps from './components/FiveSteps';
 import CompareSlider from './components/CompareSlider';
 import Footer from './components/Footer';
+import QuoteForm from './components/form';
+import Header from './components/nav_bar';
+import Navbar from './components/nav_bar';
+import AboutUs from './components/about_us';
 
   function App() {
     return (
       <div className="App">
+      <Header/>
+      <QuoteForm/>
+      <AboutUs/>
       <OurServices />
       <WhyUs />
       <FiveSteps />
->>>>>>> e4f15571efe90efcffb75417026ee83251e94322
       <CompareSlider/>
-      <QuoteForm/>
-      <div> <Header/></div>
-      <AboutUs/>
       <Footer/>
-    
     </div>
   );
 }
