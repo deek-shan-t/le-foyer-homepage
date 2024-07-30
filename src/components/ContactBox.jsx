2d457cd1262db44ad73a09aba6c6a883517e40de
Dikshant jha <mvdikshant@gmail.com>
2024-07-30 06:34:40 +0530

    Design_Stories is added

--- src/components/ContactBox.jsx
+++ src/components/ContactBox.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import './Contact_box.css';
+import img1 from '../pngwing.com (18).png';
+import img2 from '../pngwing.com (15).png';
+import img3 from '../pngwing.com (20).png';
+import img4 from '../pngwing.com (19).png';
 function Contact_box() {
   return (
     <>
@@ -11,6 +15,12 @@ Hinjawadi, Pune, Pimpri-Chinchwad,
 Maharashtra 411057</p>
 <p className=' text_style justify underline'>077969 73767</p>
 <p className='text_style justify'>inquiryl@lefoyerdecor.com</p>
+<div id='imgbox'>
+  <img src={img1} alt="" />
+  <img src={img2} alt="" />
+  <img src={img3} alt="" />
+  <img src={img4} alt="" />
+</div>
 
     </div>
     </>
---
+++ src/components/Image_gallery.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Image_gallery.css';
+import imggallery from './handover.png';
+
+function Image_gallery() {
+  return (
+    <>
+      <div id='image_gallery_heading'>
+        <h3 className='center'>
+          Image Gallery
+        </h3>
+      </div>
+      <div className="container">
+        {[...Array(9)].map((_, index) => (
+          <img src={imggallery} alt={`Gallery ${index + 1}`} key={index} className="gallery-image" />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Image_gallery;
